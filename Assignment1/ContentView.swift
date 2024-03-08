c98ffe8115ec5a858fa57f3d23e845930516effd
julienlegault <julien.p.legault@gmail.com>
2024-03-07 16:07:49 -0800

    Completed Project

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -1,26 +1,72 @@
 //
-// ContentView.swift : Assignment1
+//  ContentView.swift
+//  CheckoutApp
 //
-// Copyright © 2023 Auburn University.
-// All Rights Reserved.
-
+//  Created by Julien on 3/7/24.
+//
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojiItems = [
+        "\u{1f60a}",
+        "\u{1F574}",
+        "\u{1F4DF}",
+        "\u{1F58B}",
+        "\u{303D}",
+        "\u{1F38D}",
+        "\u{1F3E9}",
+        "\u{1F300}",
+        "\u{1F925}",
+        "\u{1F7E5}"
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(emojiItems, id: \.self) { icon in
+                    ListItem(emoji: icon)
+                }
+            }
+            .listStyle(.grouped)
+            .navigationTitle("Emoji Counter")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+#Preview {
+    ContentView()
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
+
+#Preview {
+    ContentView()
+        .preferredColorScheme(.light)
+}
+
+struct ListItem: View {
+
+    var emoji: String
+    @State var counter = 0
+    
+    var body: some View {
+        HStack {
+            Text(String("\(emoji)"))
+            Text("Counter: \(counter)")
+            Spacer()
+            Image(systemName: "plus")
+                .imageScale(.large)
+                .onTapGesture {
+                    counter += 1
+                }
+            Image(systemName: "minus")
+                .imageScale(.large)
+                .onTapGesture {
+                    counter -= 1
+                }
+        }
+        .padding()
+        .foregroundStyle(.tint)
     }
 }
